3937f5cf92d672fedc5a8445a4b2c89cbf876de1
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-11-01 20:45:05 -0400

    update thought-routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,6 +1,26 @@
 const { Thought, User } = require('../models');
 
 const thoughtController = {
+    //get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .select('-__v')
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+        //Get single thought by its _id
+        getThoughtById({ params }, res) {
+            Thought.findOne({ _id: params.id })
+                .select('-__v')
+                .then(dbThoughtData => res.json(dbThoughtData))
+                .catch(err => {
+                    console.log(err);
+                    res.sendStatus(400);
+                });
+        },
     // add thought
     addThought({ params, body }, res) {
         console.log(body);
@@ -20,6 +40,17 @@ const thoughtController = {
                 res.json(dbUserData);
             })
             .catch(err => res.json(err));
+    },
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id }, { $set: body }, { new: true, runValidators: true })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(400).json(err));
     },
 
     // add reaction to comment
